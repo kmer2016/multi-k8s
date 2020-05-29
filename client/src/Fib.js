6950d129d24942cbaef2692cccd434068e4329f8
chaka <ngamenichaka@gmail.com>
2020-05-30 00:16:26 +0200

    start dev version in minikube cluster

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: "",
+    index: ''
   };
 
   componentDidMount() {
@@ -14,28 +14,28 @@ class Fib extends Component {
   }
 
   async fetchValues() {
-    const values = await axios.get("/api/values/current");
+    const values = await axios.get('/api/values/current');
     this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get("/api/values/all");
+    const seenIndexes = await axios.get('/api/values/all');
     this.setState({
-      seenIndexes: seenIndexes.data,
+      seenIndexes: seenIndexes.data
     });
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    await axios.post("/api/values", {
-      index: this.state.index,
+    await axios.post('/api/values', {
+      index: this.state.index
     });
-    this.setState({ index: "" });
+    this.setState({ index: '' });
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(", ");
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
@@ -59,7 +59,7 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={(event) => this.setState({ index: event.target.value })}
+            onChange={event => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
     <div>
-      Im some other page ! <Link to="/">Go Back home</Link>
+      Im some other page!
+      <Link to="/">Go back home</Link>
     </div>
   );
 };
